97fbe83531a67baad6001fcfa33d777075face10
hingel <henrik.ingelsten@iths.se>
2023-10-22 12:09:30 +0200

    - Refactored

---
+++ Services/Extensions.cs
@@ -0,0 +1,34 @@
+﻿namespace Services;
+
+public static class Extensions
+{ 
+	public static void PrintChapterList(List<int> list)
+	{
+		Console.Clear();
+		Console.WriteLine("Current path:");
+
+		Console.Write(list[0]);
+		for (int i = 1; i < list.Count; i++)
+		{
+			Console.Write(" -> " + list[i]);
+		}
+
+		Console.WriteLine();
+	}
+
+	public static int[] InputCheck()
+	{
+		var stringArr = Console.ReadLine().Replace(" ", "").Split(',').ToArray();
+		var intArr = new List<int>();
+
+		foreach (var s in stringArr)
+		{
+			if (int.TryParse(s, out var testInt))
+			{
+				intArr.Add(testInt);
+			}
+		}
+
+		return intArr.ToArray();
+	}
+}
